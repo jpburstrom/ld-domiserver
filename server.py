4e5dec5d45b414fbb0e2f2c9b899edf4523baa9e
Johannes Burström <johannes@ljud.org>
2017-11-21 10:04:03 +0100

    fix indentation

--- server.py
+++ server.py
@@ -82,12 +82,12 @@ def get_local_ip():
 
 class GracefulKiller:
     kill_now = False
-  def __init__(self):
-      signal.signal(signal.SIGINT, self.exit_gracefully)
-    signal.signal(signal.SIGTERM, self.exit_gracefully)
+    def __init__(self):
+        signal.signal(signal.SIGINT, self.exit_gracefully)
+        signal.signal(signal.SIGTERM, self.exit_gracefully)
 
-  def exit_gracefully(self,signum, frame):
-      self.kill_now = True
+    def exit_gracefully(self,signum, frame):
+        self.kill_now = True
 
 
 if __name__ == "__main__":
